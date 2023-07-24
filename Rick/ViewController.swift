36d2af6c541de00fff9537c323ea27fbbc3d85f3
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-07-24 18:08:47 +0300

    Initial Commit

---
+++ Rick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick
+//
+//  Created by Антон Павлов on 24.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
