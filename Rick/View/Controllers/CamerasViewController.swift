38124a80e58a1af31af8bd57abe1973565299acb
Anton Pavlov <toni.pavlov.85@bk.ru>
2023-07-30 01:29:46 +0300

    Change something

--- Rick/View/Controllers/CamerasViewController.swift
+++ Rick/View/Controllers/CamerasViewController.swift
@@ -15,7 +15,7 @@ final class CamerasViewController: UIViewController {
     private let networkService = NetworkService()
     private var realmData: Results<CamerasRealm>!
     private var networkData: DataModel?
-    private var isInternetAviable = false
+    private var isInternetAviable = true
     
     // MARK: - Outlets
     
@@ -86,8 +86,8 @@ extension CamerasViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CamerasCell", for: indexPath) as? CamerasCell else {
             return UITableViewCell()
         }
-        if isInternetAviable == true {
-            cell.configureFromNet(networkData!, at: indexPath)
+        if isInternetAviable, let networkData = networkData {
+            cell.configureFromNet(networkData, at: indexPath)
         } else {
             cell.configureFromRealm(realmData[indexPath.row])
         }
